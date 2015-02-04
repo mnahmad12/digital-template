af0d15a4b82e474d5401bef856bb8c55c055fbfc
Mohammad Ahmad <mnahmad12@gmail.com>
2015-02-04 13:41:22 -0500

    States!

--- js/main.js
+++ js/main.js
@@ -14,156 +14,225 @@ window.onload = function() {
     "use strict";
     
 	
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
-    
-    function preload() {
-        //pre-loading the zombies
-		game.load.spritesheet('bob', 'assets/phaser-dude.png', 37, 45, 18);
-		game.load.spritesheet('mummy', 'assets/metalslug_mummy37x45.png', 37, 45, 18);
+	var P2Game = {};
+	
+	P2Game.StateA = function (game) {
+
+		this.message;
+		this.cursors;
+		this.logo;
+
+	};
+	
+	P2Game.StateA.prototype = 
+	{
 		
+		
+		 gotoStateB: function () 
+		{
+
+			this.state.start('StateB');
+
+		},
+		
+		update: function()
+		{
+			this.game.stage.backgroundColor = '#806000';
+			this.cursors = this.input.keyboard.createCursorKeys();
+			if (this.cursors.left.isDown)
+			{
+				this.gotoStateB();
+			}
+		}
+	
+	}
+	
+	
+	P2Game.StateC = function (game) {
+
+		this.message;
+		this.cursors;
+		this.logo;
+		this.text;
+		this.style;
+
+	};
+	
+	P2Game.StateC.prototype = 
+	{
+		
+		
+		 gotoStateA: function () 
+		{
+
+			this.state.start('StateA');
+
+		},
+		
+		update: function()
+		{
+			this.game.stage.backgroundColor = '#806000';
+			this.cursors = this.input.keyboard.createCursorKeys();
+			
+			this.style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+			this.text = this.add.text( this.world.centerX, 15, "You Lose! Want to Play Again? Press The Left Arrow!", this.style );
+			this.text.anchor.setTo( 0.5, 0.0 );
+			
+			if (this.cursors.left.isDown)
+			{
+				this.gotoStateA();
+			}
+		}
+	
 	}
     
-
-	var mummies;
-	var bob;
-	var keys;
-	var text;
-	var style;
+	
+	P2Game.StateB = function(game)
+		{
+			this.mummies;
+			this.bob;
+			this.keys;
+			this.text;
+			this.style;
+		}
+	
+	P2Game.StateB.prototype=
+	{
+		preload: function() 
+		{
+        //pre-loading the zombies
+			this.load.spritesheet('bob', 'assets/phaser-dude.png', 28, 28, 18);
+			this.load.spritesheet('mummy', 'assets/metalslug_mummy37x45.png', 37, 45, 15);
+			this.game.stage.backgroundColor = '#000000';
+		},
+    
 
 	
-    function create() {
-        
-		
-		
+		create: function() 
+		{
+			
+			
 		//starting physics:
-		game.physics.startSystem(Phaser.Physics.ARCADE);
+			this.game.physics.startSystem(Phaser.Physics.ARCADE);
 		
 		//creating mummies
-		mummies = game.add.group();
+			this.mummies = this.add.group();
 		//and bob
-		bob = game.add.sprite(300,100,'bob');
+			this.bob = this.add.sprite(300,100,'bob');
 		
 		
 		
 		//Creating 10 mummies, each initially dead
-		mummies.createMultiple(20,"mummy",0,false);
+			this.mummies.createMultiple(20,"mummy",0,false);
 		
 		
+			this.game.physics.enable(this.mummies,Phaser.Physics.ARCADE);
+			this.game.physics.enable(this.bob,Phaser.Physics.ARCADE);
 		
 		//bringing mummies to life!
-		game.time.events.repeat(Phaser.Timer.SECOND, 20, resurrect, this);
+			
+			this.game.time.events.repeat(Phaser.Timer.SECOND, 20, this.resurrect,this );
  
         
       
-		game.physics.enable(mummies,Phaser.Physics.ARCADE);
-		game.physics.enable(bob,Phaser.Physics.ARCADE);
+			
+	
 		
-       
-    
-		
-        
-		
-        
-		
-		
-    }
+		},
 	
 	
 	
-	function resurrect() {
-
-		//  Get a dead item
-		var item = mummies.getFirstDead();
-
-		if (item)
+		resurrect: function()
 		{
-			//  And bring it back to life
-			item.reset(game.world.randomX, game.world.randomY);
+			
+		 //Get a dead item
+			var item = this.mummies.getFirstDead();
 
-			//  This just changes its frame
-			item.frame = game.rnd.integerInRange(0, 36);
+			if (item)
+			{
+			 //And bring it back to life
+				item.reset((this.world.randomX + (this.bob.x)), (this.world.randomY+(this.bob.y)));
+
+			 //This just changes its frame
+				item.frame = this.rnd.integerInRange(0, 36);
 			
 			//getting the mummies to run
-			item.animations.add('walk');
+				item.animations.add('walk');
 
-			item.animations.play('walk', 30, true);
+				item.animations.play('walk', 30, true);
 			
 			//and move towards cursor
-			item.rotation = game.physics.arcade.moveToObject( item,bob, 100 );
-		}
+				item.rotation = this.physics.arcade.moveToObject( item,this.bob, 100 );
+			}
 
-	}
+		},
 	
-	function update(){
-	
-		//adding some text with instructions:
+		update: function()
+		{
+			//window.alert("here?");
+		
 		// Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
-        style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        text = game.add.text( game.world.centerX, 15, "Don't Let the Zombies Get You!.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+			this.style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+			this.text = this.add.text( this.world.centerX, 15, "Don't Let the Mummies Get You!.", this.style );
+			this.text.anchor.setTo( 0.5, 0.0 );
 		
-		keys = game.input.keyboard.createCursorKeys();
 		
-		if(bob.exists)
+			this.keys = this.input.keyboard.createCursorKeys();
+		
+			if(this.bob.exists)
+			{
+				
+				this.bob.body.velocity.x=0;
+				this.bob.body.velocity.y=0;
+				
+				if (this.keys.left.isDown)
+				{
+					this.bob.body.velocity.x=-200;	
+				}
+			
+				if (this.keys.right.isDown)
+				{
+					this.bob.body.velocity.x=200;	
+				}
+			
+				if (this.keys.up.isDown)
+				{	
+					this.bob.body.velocity.y=-200;	
+				}
+			
+				if (this.keys.down.isDown)
+				{	
+					this.bob.body.velocity.y=200;
+				}
+		
+				if(this.physics.arcade.collide(this.mummies,this.bob))
+				{
+					this.bob.kill();
+					this.bob.exists=false;	
+					this.goToStateC();
+				}
+			
+			}
+				
+			
+		
+		},
+		
+		goToStateC: function () 
 		{
-			bob.body.velocity.x=0;
-			bob.body.velocity.y=0;
-			
-			if (keys.left.isDown)
-			{
-				
-				
-				bob.body.velocity.x=-200;
-				
-			}
-			
-			if (keys.right.isDown)
-			{
-				
-				bob.body.velocity.x=200;
-				
-			}
-			
-			if (keys.up.isDown)
-			{
-				
-				bob.body.velocity.y=-200;
-				
-			}
-			
-			if (keys.down.isDown)
-			{
-				
-				bob.body.velocity.y=200;
-				
-			}
-		
-			if(game.physics.arcade.overlap(mummies,bob,null,null,this))
-			{
-				
-				bob.kill();
-				bob.exists=false;
-				
-				endGame();
-				
-			}
-		
+
+			this.state.start('StateC');
+
 		}
-				
-			
-		
 	}
-	
-	function endGame()
-	{
-		
-		
-		
-		style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-		text=game.add.text( game.world.centerX, 15, "Game Over.", style );
-		text.anchor.setTo( 0.5, 0.0 );
-	}
+	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
+    
+	game.state.add('StateA', P2Game.StateA);
+	game.state.add('StateB', P2Game.StateB);
+	game.state.add('StateC', P2Game.StateC);
+
+	game.state.start('StateA');
     
     
-};
+ };
