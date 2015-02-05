84871730f4d0d91d6eb8f8b438cc260727c302f6
Mohammad Ahmad <mnahmad12@gmail.com>
2015-02-05 11:11:03 -0500

    Key moving around, more zombies, faster zombies

--- js/main.js
+++ js/main.js
@@ -65,6 +65,40 @@ window.onload = function() {
 	}
 	
 	
+	P2Game.StateD=function(game){
+		this.cursors;
+		this.style;
+		this.text;
+	}
+	
+	P2Game.StateD.prototype=
+	{
+		gotoStateA: function () 
+		{
+
+			this.state.start('StateA');
+
+		},
+		
+		update: function()
+		{
+			
+			this.cursors = this.input.keyboard.createCursorKeys();
+			
+			this.style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
+			this.text = this.add.text( this.world.centerX, 15, "You Win!\nTo play the next level please\nSend your credit card info to mahmad15@gmu.edu\n(To Restart press the left arrow)", this.style );
+			this.text.anchor.setTo( 0.5, 0.0 );
+			
+			if (this.cursors.left.isDown)
+			{
+				this.gotoStateA();
+			}
+			
+			
+		}
+	
+	}
+	
 	P2Game.StateC = function (game) {
 
 		this.message;
@@ -79,12 +113,13 @@ window.onload = function() {
 	{
 		
 		
-		 gotoStateA: function () 
+		 gotoStateB: function () 
 		{
 
-			this.state.start('StateA');
+			this.state.start('StateB');
 
 		},
+		
 		
 	
 		
@@ -99,8 +134,9 @@ window.onload = function() {
 			
 			if (this.cursors.left.isDown)
 			{
-				this.gotoStateA();
+				this.gotoStateB();
 			}
+		
 		}
 	
 	}
@@ -142,12 +178,13 @@ window.onload = function() {
 			this.mummies = this.add.group();
 		//and bob
 			this.bob = this.add.sprite(300,100,'bob');
-		//adding the running dog!
+		//adding the key
 			this.key=this.add.sprite(600,200,'key');
-		
+			this.key.exists=false;
+			
 		
 		//Creating 10 mummies, each initially dead
-			this.mummies.createMultiple(20,"mummy",0,false);
+			this.mummies.createMultiple(50,"mummy",0,false);
 		
 			this.game.physics.enable(this.key,Phaser.Physics.ARCADE);
 			this.game.physics.enable(this.mummies,Phaser.Physics.ARCADE);
@@ -155,12 +192,13 @@ window.onload = function() {
 		
 		//bringing mummies to life!
 			
-			this.game.time.events.repeat(Phaser.Timer.SECOND, 20, this.resurrect,this );
- 
+			this.game.time.events.repeat(Phaser.Timer.SECOND/2, 100, this.resurrect,this );
+			this.game.time.events.repeat(Phaser.Timer.SECOND,100,this.randKey,this)
  
         //getting the key to move around and bounce
 			this.key.body.collideWorldBounds=true;
 			this.key.body.velocity.setTo(200,200);
+			
 			this.physics.arcade.collide(this.mummies,this.key);
 			this.key.body.bounce.set(1);
 	
@@ -168,7 +206,7 @@ window.onload = function() {
 			// Add some text using a CSS style.
         // Center it in X, and position its top 15 pixels from the top of the world.
 			this.style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-			this.text = this.add.text( this.world.centerX, 15, "Don't Let the Mummies Get You!.", this.style );
+			this.text = this.add.text( this.world.centerX, 15, "Don't Let the Monsters Get You!.", this.style );
 			this.text.anchor.setTo( 0.5, 0.0 );
 		
 		
@@ -177,14 +215,27 @@ window.onload = function() {
 			
 		},
 	
+		randKey: function(){
+			
+			if(!this.key.exists)
+			{
+				this.key.exists=true;
+			}
+			
+			this.key.x=this.rnd.integerInRange(0,800);
+			this.key.y=this.rnd.integerInRange(0,600);
+			
+		},
 	
 	
 		resurrect: function()
 		{
 			
+		
+			
 		 //Get a dead item
 			var item = this.mummies.getFirstDead();
-
+			
 			if (item)
 			{
 			 //And bring it back to life
@@ -199,10 +250,11 @@ window.onload = function() {
 				item.animations.play('walk', 30, true);
 			
 			//and move towards cursor
-				item.rotation = this.physics.arcade.moveToObject( item,this.bob, 100+this.rnd.integerInRange(10,200) );
+				item.rotation = this.physics.arcade.moveToObject( item,this.bob, 200+this.rnd.integerInRange(10,200) );
 				item.checkWorldBounds=true;
 			}
 
+			
 		},
 	
 		update: function()
@@ -244,6 +296,11 @@ window.onload = function() {
 				this.goToStateC();
 			}
 			
+			if(this.physics.arcade.overlap(this.bob,this.key,null,null,this))
+			{
+				this.goToStateD();
+			}
+			
 			
 				
 			
@@ -255,6 +312,13 @@ window.onload = function() {
 
 			this.state.start('StateC');
 
+		},
+		
+		goToStateD: function () 
+		{
+
+			this.state.start('StateD');
+
 		}
 	}
 	var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
@@ -262,7 +326,7 @@ window.onload = function() {
 	game.state.add('StateA', P2Game.StateA);
 	game.state.add('StateB', P2Game.StateB);
 	game.state.add('StateC', P2Game.StateC);
-
+	game.state.add('StateD', P2Game.StateD);
 	game.state.start('StateA');
     
     
